e85bb9d180907dbf699b9dc84542a7aa7c6639af
Ben E <benbjammin@gmail.com>
2015-06-18 22:23:35 -0600

    Add .Net solution files/folders

---
+++ ReactDemo/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using ReactDemo.Models;
+
+namespace ReactDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
+
+        private static readonly IList<CommentModel> _comments;
+        
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[OutputCache(Location = OutputCacheLocation.None)]
+        public ActionResult Comments()
+        {
+            return Json(_comments, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
